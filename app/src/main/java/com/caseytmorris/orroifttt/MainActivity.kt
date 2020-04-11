261cf809b11c751f798b7da212f979af235b5ec1
casey <casey@getorro.com>
2020-04-11 14:46:42 -0500

    Add simple buttons to view

--- app/src/main/java/com/caseytmorris/orroifttt/MainActivity.kt
+++ app/src/main/java/com/caseytmorris/orroifttt/MainActivity.kt
@@ -2,11 +2,20 @@ package com.caseytmorris.orroifttt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setOnClickListeners()
+    }
+
+    fun setOnClickListeners() {
+        turn_on_button.setOnClickListener { Log.i("TurnOnClick",  "Casey On Clicked") }
+
+        turn_off_button.setOnClickListener { Log.i("TurnOffClick","Casey OffClicked") }
     }
 }
