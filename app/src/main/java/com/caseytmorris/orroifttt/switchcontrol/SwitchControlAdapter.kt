2820391894f15b719701d735b8582b6f291e11bc
casey <casey@getorro.com>
2020-04-13 11:05:05 -0500

    Add click listeners to the buttons in the RecyclerViews. Works

--- app/src/main/java/com/caseytmorris/orroifttt/switchcontrol/SwitchControlAdapter.kt
+++ app/src/main/java/com/caseytmorris/orroifttt/switchcontrol/SwitchControlAdapter.kt
@@ -1,22 +1,22 @@
 package com.caseytmorris.orroifttt.switchcontrol
 
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.caseytmorris.orroifttt.R
-import com.caseytmorris.orroifttt.TextItemViewHolder
 import com.caseytmorris.orroifttt.database.RoomControl
-import com.caseytmorris.orroifttt.databinding.ListItemRoomControlBinding
-import com.caseytmorris.orroifttt.formatRoom
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class SwitchControlAdapter : ListAdapter<RoomControl,SwitchControlAdapter.ViewHolder>(SwitchControlDiffCallback()) {
 
@@ -32,9 +32,18 @@ class SwitchControlAdapter : ListAdapter<RoomControl,SwitchControlAdapter.ViewHo
 
     class ViewHolder private constructor(itemView: View): RecyclerView.ViewHolder(itemView) {
         val roomName: TextView = itemView.findViewById(R.id.room_name)
+        val onButton: Button = itemView.findViewById(R.id.button_turn_on_list)
+        val offButton: Button = itemView.findViewById(R.id.button_turn_off_list)
 
         fun bind(item: RoomControl) {
             roomName.text = item.roomName
+            onButton.setOnClickListener {
+                sendIFTTTRequest(item.turnOnWebhook, it.context)
+            }
+
+            offButton.setOnClickListener {
+                sendIFTTTRequest(item.turnOffWebhook, it.context)
+            }
         }
 
         companion object {
@@ -43,6 +52,30 @@ class SwitchControlAdapter : ListAdapter<RoomControl,SwitchControlAdapter.ViewHo
                     .inflate(R.layout.list_item_room_control, parent, false)
                 return ViewHolder(view)
             }
+        }
+
+        private fun sendIFTTTRequest(event:String, context: Context){
+            val queue = Volley.newRequestQueue(context)
+            val url = "https://maker.ifttt.com/trigger/$event/with/key/${context.getString(R.string.webhooks_key)}"
+
+            // Request a string response from the provided URL.
+            val stringRequest = StringRequest(
+                Request.Method.POST, url,
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+                    Toast.makeText(context,"Success! Lighting action in progress!",
+                        Toast.LENGTH_SHORT).show()
+                },
+                Response.ErrorListener {
+                    Toast.makeText(context,"Error Processing Lighting Request!",
+                        Toast.LENGTH_SHORT).show()
+                }
+            )
+
+            Log.i("sendIFTTTRequest",  "Casey IFTTT URL: $url")
+
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest)
         }
     }
 
@@ -58,73 +91,3 @@ class SwitchControlDiffCallback : DiffUtil.ItemCallback<RoomControl>() {
         return oldItem == newItem
     }
 }
-
-//class SwitchControlAdapter(val clickListener: SwitchControlListener) : ListAdapter<DataItem,
-//        RecyclerView.ViewHolder>(SwitchControlDiffCallback()) {
-//
-//    private val adapterScope = CoroutineScope(Dispatchers.Default)
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-//        return ViewHolder.from(parent)
-//    }
-//
-//    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-//        when (holder) {
-//            is ViewHolder -> {
-//                val roomControlItem = getItem(position) as DataItem.RoomControlItem
-//                holder.bind(clickListener,roomControlItem.room)
-//            }
-//        }
-//    }
-//
-//    fun addHeaderAndSubmitList(list: List<RoomControl>?) {
-//        adapterScope.launch {
-//            val items = when (list) {
-//                null -> listOf(DataItem.Header)
-//                else -> listOf(DataItem.Header) + list.map { DataItem.RoomControlItem(it) }
-//            }
-//            withContext(Dispatchers.Main) {
-//                submitList(items)
-//            }
-//        }
-//    }
-//
-//
-//
-//    class ViewHolder private constructor(val binding: ListItemRoomControlBinding)
-//        : RecyclerView.ViewHolder(binding.root) {
-//
-//        fun bind(clickListener: SwitchControlListener, item: RoomControl) {
-//            binding.room = item
-//            binding.clickListener = clickListener
-//            binding.executePendingBindings()
-//        }
-//
-//        companion object {
-//            fun from(parent: ViewGroup) : ViewHolder {
-//                val layoutInflater = LayoutInflater.from(parent.context)
-//                val binding = ListItemRoomControlBinding.inflate(layoutInflater,parent,false)
-//                return ViewHolder(binding)
-//            }
-//        }
-//    }
-//}
-//
-//
-
-//
-//class SwitchControlListener(val clickListener: (roomId: Long) -> Unit) {
-//    fun onClick(room: RoomControl) = clickListener(room.roomId)
-//}
-//
-//sealed class DataItem {
-//    data class RoomControlItem(val room: RoomControl): DataItem() {
-//        override val id = room.roomId
-//    }
-//
-//    object Header: DataItem() {
-//        override val id = Long.MIN_VALUE
-//    }
-//
-//    abstract val id: Long
-//}
--- app/src/main/java/com/caseytmorris/orroifttt/switchcontrol/SwitchControlFragment.kt
+++ app/src/main/java/com/caseytmorris/orroifttt/switchcontrol/SwitchControlFragment.kt
@@ -43,35 +43,13 @@ class SwitchControl : Fragment() {
 
         binding.viewModel = switchControlViewModel
 
-//        binding.turnOnButton.setOnClickListener {
-//            Log.i("Casey",  "IFTTT On Clicked")
-//            val len = switchControlViewModel
-//            Log.i("Casey","length of rooms: $len")
-////            sendIFTTTRequest(getString(R.string.turn_on_webhook))
-//        }
-//
-//        binding.turnOffButton.setOnClickListener {
-//            Log.i("Casey","IFTTT Off Clicked")
-////            sendIFTTTRequest(getString(R.string.turn_off_webhook))
-//        }
-
-//        binding.buttonAddRoom.setOnClickListener(
-//            Navigation.createNavigateOnClickListener(R.id.action_switch_control_fragment_to_switchAddRoomFragment)
-//        )
+        binding.buttonAddRoom.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_switch_control_fragment_to_switchAddRoomFragment)
+        )
 
 //        binding.textViewRooms.text = switchControlViewModel.roomsString.value
 //
 //        binding.textViewWebapi.text = switchControlViewModel.apiKeyString.value
-
-//        val adapter = SwitchControlAdapter(SwitchControlListener { roomId ->
-//            switchControlViewModel.onSwitchControlRoomClicked(roomId)
-//        })
-//
-//        switchControlViewModel.rooms.observe(viewLifecycleOwner, Observer {
-//            it?.let {
-//                adapter.addHeaderAndSubmitList(it)
-//            }
-//        })
 
         val adapter = SwitchControlAdapter()
         binding.roomList.adapter = adapter
@@ -85,28 +63,5 @@ class SwitchControl : Fragment() {
         return binding.root
     }
 
-//    private fun sendIFTTTRequest(event:String){
-//        val queue = Volley.newRequestQueue(context)
-//        val url = "https://maker.ifttt.com/trigger/$event/with/key/${getString(R.string.webhooks_key)}"
-//
-//        // Request a string response from the provided URL.
-//        val stringRequest = StringRequest(
-//            Request.Method.POST, url,
-//            Response.Listener<String> { response ->
-//                // Display the first 500 characters of the response string.
-//                Toast.makeText(context,"Success! Lighting action in progress!",
-//                    Toast.LENGTH_SHORT).show()
-//            },
-//            Response.ErrorListener {
-//                Toast.makeText(context,"Error Processing Lighting Request!",
-//                    Toast.LENGTH_SHORT).show()
-//            }
-//        )
-//
-//        Log.i("sendIFTTTRequest",  "IFTTT URL: $url")
-//
-//        // Add the request to the RequestQueue.
-//        queue.add(stringRequest)
-//    }
 }
 
