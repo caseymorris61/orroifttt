335a3475e9236561a054ff77cd39d858b839fafb
casey <casey@getorro.com>
2020-04-14 08:31:16 -0500

    Add validation for API webhook key

--- app/src/main/java/com/caseytmorris/orroifttt/switcheditroom/SwitchEditRoomFragment.kt
+++ app/src/main/java/com/caseytmorris/orroifttt/switcheditroom/SwitchEditRoomFragment.kt
@@ -2,15 +2,18 @@ package com.caseytmorris.orroifttt.switcheditroom
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.caseytmorris.orroifttt.R
 import com.caseytmorris.orroifttt.database.RoomControlDatabase
 import com.caseytmorris.orroifttt.databinding.FragmentSwitchEditRoomBinding
+import com.caseytmorris.orroifttt.switchaddroom.API_KEY_VALIDATION_STATE
 import com.caseytmorris.orroifttt.switchaddroom.SwitchAddRoomViewModel
 
 /**
@@ -40,6 +43,32 @@ class SwitchEditRoomFragment : Fragment() {
         binding.setLifecycleOwner(this)
 
         binding.viewModel = switchEditRoomViewModel
+
+        switchEditRoomViewModel.webhookApiKeyLiveData.observe(this, Observer {
+            switchEditRoomViewModel.apiKeyChanged()
+        })
+
+        switchEditRoomViewModel.apiKeyValidated.observe(this, Observer { newState ->
+            when (newState) {
+                API_KEY_VALIDATION_STATE.PASS -> {
+                    Log.i("Casey","Validation Passed")
+                    binding.saveRoom.isEnabled = true
+                    binding.validateButton.isEnabled = false
+                }
+                API_KEY_VALIDATION_STATE.FAILED -> {
+                    Log.i("Casey","Validation Failed")
+                    //Prompt message that it failed to validate
+                    binding.saveRoom.isEnabled = false
+                    binding.validateButton.isEnabled = true
+                }
+                else -> {
+                    Log.i("Casey","Validation Unknown")
+                    binding.saveRoom.isEnabled = false
+                    binding.validateButton.isEnabled = true
+                }
+
+            }
+        })
 
         return binding.root
     }
