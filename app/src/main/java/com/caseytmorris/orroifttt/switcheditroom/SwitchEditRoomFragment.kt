b0d3f16146afbf82ad4be2bee4fef91b470baf11
casey <casey@getorro.com>
2020-04-14 08:46:56 -0500

    Add error toast message to failed validation

--- app/src/main/java/com/caseytmorris/orroifttt/switcheditroom/SwitchEditRoomFragment.kt
+++ app/src/main/java/com/caseytmorris/orroifttt/switcheditroom/SwitchEditRoomFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -51,18 +52,16 @@ class SwitchEditRoomFragment : Fragment() {
         switchEditRoomViewModel.apiKeyValidated.observe(this, Observer { newState ->
             when (newState) {
                 API_KEY_VALIDATION_STATE.PASS -> {
-                    Log.i("Casey","Validation Passed")
                     binding.saveRoom.isEnabled = true
                     binding.validateButton.isEnabled = false
                 }
                 API_KEY_VALIDATION_STATE.FAILED -> {
-                    Log.i("Casey","Validation Failed")
-                    //Prompt message that it failed to validate
+                    Toast.makeText(context,"Error Validating Webhook API Key",
+                        Toast.LENGTH_SHORT).show()
                     binding.saveRoom.isEnabled = false
                     binding.validateButton.isEnabled = true
                 }
                 else -> {
-                    Log.i("Casey","Validation Unknown")
                     binding.saveRoom.isEnabled = false
                     binding.validateButton.isEnabled = true
                 }
