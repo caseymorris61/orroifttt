44c406fd4d284bb4f13faf196513de4bdc22d233
casey <casey@getorro.com>
2020-04-16 16:49:41 -0500

    Add info to about and setup fragments

--- app/src/main/java/com/caseytmorris/sparkdirector/menus/AboutFragment.kt
+++ app/src/main/java/com/caseytmorris/sparkdirector/menus/AboutFragment.kt
@@ -1,11 +1,14 @@
 package com.caseytmorris.sparkdirector.menus
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.caseytmorris.sparkdirector.R
 
 /**
@@ -18,8 +21,17 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val view: View? = inflater.inflate(R.layout.fragment_about, container, false)
+
+        val learnMoreButton: Button? = view?.findViewById(R.id.learn_more_button)
+        learnMoreButton?.setOnClickListener { goToURL("http://caseytmorris.com/sparkdirector") }
+
+        return view
     }
 
-
+    private fun goToURL(url:String) {
+        val uriURL = Uri.parse(url)
+        val  launchBrowser = Intent(Intent.ACTION_VIEW,uriURL)
+        startActivity(launchBrowser)
+    }
 }
--- app/src/main/java/com/caseytmorris/sparkdirector/menus/SetupFragment.kt
+++ app/src/main/java/com/caseytmorris/sparkdirector/menus/SetupFragment.kt
@@ -1,9 +1,13 @@
 package com.caseytmorris.sparkdirector.menus
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.caseytmorris.sparkdirector.R
 
@@ -14,7 +18,18 @@ class SetupFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setup, container, false)
+        val view: View? = inflater.inflate(R.layout.fragment_setup, container, false)
+
+        val goButton: Button? = view?.findViewById(R.id.go_button)
+        goButton?.setOnClickListener { goToURL("http://caseytmorris.com/sparkdirector/setup") }
+
+        return view
+    }
+
+    private fun goToURL(url:String) {
+        val uriURL = Uri.parse(url)
+        val  launchBrowser = Intent(Intent.ACTION_VIEW,uriURL)
+        startActivity(launchBrowser)
     }
 
 
